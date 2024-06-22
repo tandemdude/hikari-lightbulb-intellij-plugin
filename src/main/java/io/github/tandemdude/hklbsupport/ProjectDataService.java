76326e2f92b6ba9c21326ad61c9d74a7c14b4210
tandemdude <olijdthompson@gmail.com>
2024-06-22 23:04:24 +0100

    Fix cache not being repopulated after being cleared + add license headers

--- src/main/java/io/github/tandemdude/hklbsupport/ProjectDataService.java
+++ src/main/java/io/github/tandemdude/hklbsupport/ProjectDataService.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2024-present tandemdude
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package io.github.tandemdude.hklbsupport;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -56,48 +78,45 @@ public final class ProjectDataService {
         return new LightbulbData(version, parsedParamData);
     }
 
-    public void notifyChange(Sdk sdk) {
-        if (!sdkCache.containsKey(sdk)) {
-            return;
+    boolean populateCacheForSdk(Sdk sdk, VirtualFile paramsFile) {
+        if (!paramsFile.getName().equals("metaparams.json")
+                || !paramsFile.getParent().getName().equals("lightbulb")) {
+            return true;
         }
 
+        var initFile = paramsFile.getParent().findChild("__init__.py");
+        if (initFile == null) {
+            return true;
+        }
+
+        try {
+            var matcher = VERSION_PATTERN.matcher(new String(initFile.contentsToByteArray()));
+            String version = null;
+            while (matcher.find()) {
+                version = matcher.group(1);
+            }
+
+            if (version == null) {
+                return true;
+            }
+
+            if (sdkCache.get(sdk) != null && version.equals(sdkCache.get(sdk).version())) {
+                return false;
+            }
+
+            var data = readMetaparamsFile(version, paramsFile);
+            sdkCache.put(sdk, data);
+            Notifier.notifyInformation(project, "Lightbulb configuration loaded successfully (%s)", sdk.getName());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return false;
+    }
+
+    public void notifyChange(Sdk sdk) {
         FileTypeIndex.processFiles(
                 FileTypeManager.getInstance().getFileTypeByExtension("json"),
-                file -> {
-                    if (!file.getName().equals("metaparams.json")
-                            || !file.getParent().getName().equals("lightbulb")) {
-                        return true;
-                    }
-
-                    var initFile = file.getParent().findChild("__init__.py");
-                    if (initFile == null) {
-                        return true;
-                    }
-
-                    try {
-                        var matcher = VERSION_PATTERN.matcher(new String(initFile.contentsToByteArray()));
-                        String version = null;
-                        while (matcher.find()) {
-                            version = matcher.group(1);
-                        }
-
-                        if (version == null) {
-                            return true;
-                        }
-
-                        if (version.equals(sdkCache.get(sdk).version())) {
-                            return false;
-                        }
-
-                        var data = readMetaparamsFile(version, file);
-                        sdkCache.put(sdk, data);
-                        Notifier.notifyInformation(
-                                project, "Lightbulb configuration loaded successfully (%s)", sdk.getName());
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    return false;
-                },
+                file -> populateCacheForSdk(sdk, file),
                 GlobalSearchScope.allScope(project));
     }
 }
